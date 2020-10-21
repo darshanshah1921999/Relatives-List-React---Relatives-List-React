880497eac4c4457e624f58da31885de4a3d0b909
darshanshah1921999 <54939839+darshanshah1921999@users.noreply.github.com>
2020-10-21 17:47:40 +0530

    Add relative list

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-           <>
-//write your code here
-	</>
-        )
-    }
+  render() {
+    const relatives = ["Darshan", "Prashil", "Prerak", "Jimit", "Shubham"];
+    return (
+      <>
+        <ol>
+          {relatives.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ol>
+      </>
+    );
+  }
 }
-
 
 export default App;
